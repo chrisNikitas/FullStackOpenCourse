39759efe53861453367088a952e444ae5a8dcc27
Christos Nikitas <christos.nikitas38@thehutgroup.com>
2025-03-24 19:59:54 +0000

    Part 3 backend servers

--- Part 3/phonebook-backend/index.js
+++ Part 3/phonebook-backend/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 app = express();
+app.use(cors());
 
 morgan.token("body", function (req, res) {
   return JSON.stringify(req.body);
